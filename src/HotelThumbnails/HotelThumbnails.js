8a8bf9979cad139022d9a039d739014b99c9d80d
Daniel Coats <d0coat01@louisville.edu>
2018-08-04 11:06:32 -0700

    Added nearest airport calculation to hotel thumbnail
    - Also fixed select button background-color on hover.

--- src/HotelThumbnails/HotelThumbnails.js
+++ src/HotelThumbnails/HotelThumbnails.js
@@ -1,42 +1,46 @@
 import React from 'react';
 import './HotelThumbnails.css';
 import HotelsApi from '../apis/HotelsApi';
-
+import { distance } from '../utils/location';
 const HotelThumbnail = (props) => {
   const backgroundImage = {
     background: 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3)), url(' + props.hotel.thumbnail_url + ')',
   };
   const { rating_label, ecstasy, ecstasyColor } = HotelsApi.getRatingLabel(props.hotel.ecstasy);
+  const nearest_dist = distance(props.hotel.loc[0], props.hotel.loc[1],
+    props.hotel.nearest_airport.latitude, props.hotel.nearest_airport.longitude).toFixed(1);
   const ecstasyStyle = {
     backgroundColor: ecstasyColor
   };
 
   return (
-  <li className='hotel-thumbnail' style={backgroundImage}>
-    <div className='hotel-thumbnail__details'>
-      <div className='hotel-thumbnail__name'>{props.hotel.name}</div>
-      <div className='hotel-thumbnail__rating'>
-        <span className='hotel-thumbnail__ecstasy' style={ecstasyStyle}>{ecstasy}</span>
-        <span className='hotel-thumbnail__rating-label'>{rating_label}</span>
+    <li className='hotel-thumbnail' style={backgroundImage}>
+      <div className='hotel-thumbnail__details'>
+        <div className='hotel-thumbnail__name'>{props.hotel.name}</div>
+        <div className='hotel-thumbnail__rating'>
+          <span className='hotel-thumbnail__ecstasy' style={ecstasyStyle}>{ecstasy}</span>
+          <span className='hotel-thumbnail__rating-label'>{rating_label}</span>
+        </div>
+        <div className='hotel-thumbnail__nearest_poi'>
+          {nearest_dist} mi from {props.hotel.nearest_airport.name}
+        </div>
+        <div className='hotel-thumbnail__select-container'>
+          <span className='hotel-thumbnail__select'>Select</span>
+          <span className='hotel-thumbnail__arrow'>&#9660;</span>
+        </div>
       </div>
-      <div className='hotel-thumbnail__nearest_poi'>0.1 mi from Starbucksssssssssssssssssasssssdf</div>
-      <div className='hotel-thumbnail__select-container'>
-        <span className='hotel-thumbnail__select'>Select</span>
-        <span className='hotel-thumbnail__arrow'>&#9660;</span>
-      </div>
-    </div>
-  </li>
+    </li>
   );
 }
 
 export default function HotelThumbnails(props) {
-    return (
-      <ul className='hotel-thumbnails'>
-        {
+  return (
+    <ul className='hotel-thumbnails'>
+      {
         props.hotels.map((hotel, index) => {
           return (<HotelThumbnail hotel={hotel} key={index} />)
         })
-        }
-      </ul>
-    );
+      }
+    </ul>
+  );
 }
