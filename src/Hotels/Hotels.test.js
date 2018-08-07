c2e2095f1d12d0d02b4b0de5b82d5e3ca7f678ce
Daniel Coats <d0coat01@louisville.edu>
2018-08-06 20:59:23 -0700

    Removed Smoke Tests for now

--- src/Hotels/Hotels.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hotels from './Hotels';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Hotels />, div);
-});
